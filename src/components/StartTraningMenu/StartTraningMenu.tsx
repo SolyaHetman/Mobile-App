22049cb574be5c568f7143b244dae8229f2d62c9
SolyaHetman <solyahteman2013@gmail.com>
2021-05-06 21:52:57 +0300

    finish styling for training page

--- src/components/StartTraningMenu/StartTraningMenu.tsx
+++ src/components/StartTraningMenu/StartTraningMenu.tsx
@@ -4,30 +4,32 @@ import './StartTraningMenu.css';
 
 const StartTrainingMenu : React.FC = () => (
   <>
+  <div className="trainingMenu">
     <IonCard className="cardExercise">
-      <IonCardHeader>
-        <IonCardTitle>Вправи для очей</IonCardTitle>
-        <IonToggle color="danger"/>
+      <IonCardHeader className="cardExerciseContent">
+        <IonCardTitle className="cardExerciseTitle">Вправи для очей</IonCardTitle>
+        <IonToggle />
       </IonCardHeader>
     </IonCard>
     <IonCard className="cardExercise">
-      <IonCardHeader>
-        <IonCardTitle>Вправи для шиї</IonCardTitle>
-        <IonToggle color="danger"/>
+      <IonCardHeader className="cardExerciseContent">
+        <IonCardTitle className="cardExerciseTitle">Вправи для шиї</IonCardTitle>
+        <IonToggle/>
       </IonCardHeader>
     </IonCard>
     <IonCard className="cardExercise">
-      <IonCardHeader>
-        <IonCardTitle>Вправи для спини</IonCardTitle>
-        <IonToggle color="danger"/>
+      <IonCardHeader className="cardExerciseContent">
+        <IonCardTitle className="cardExerciseTitle">Вправи для спини</IonCardTitle>
+        <IonToggle />
       </IonCardHeader>
     </IonCard>
     <IonCard className="cardExercise">
-      <IonCardHeader>
-        <IonCardTitle>Комплекс вправ</IonCardTitle>
-        <IonToggle color="danger"/>
+      <IonCardHeader className="cardExerciseContent">
+        <IonCardTitle className="cardExerciseTitle">Комплекс вправ</IonCardTitle>
+        <IonToggle />
       </IonCardHeader>
     </IonCard>
+    </div>
   </>
 );
 
